d3cf551a7c1795a48bc048d664d9cc9ad1b3d59b
David Riedl <davidriedl1991@gmail.com>
2013-07-11 00:25:44 +0200

    Dialog update
    minor changes to the license and package name

--- at/codecomb/android/dialog/DialogListener.java
+++ at/codecomb/android/dialog/DialogListener.java
@@ -1,4 +1,27 @@
-package at.ac.uibk.dialog;
+package at.codecomb.android.dialog;
+
+/*
+ * Copyright (c) 2013, All Rights Reserved, file = PausableThread.java
+ * 
+ * This source is subject to Code Comb. 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /**
  * Activities which start GenericDialogs using show must implement this interface when the GenericDialog is set to be listenable. The GenericDialog will set the Activity as listener in onAttach and
